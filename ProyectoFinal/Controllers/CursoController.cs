66c6ea4ceb9bb14af30b38106698b38446bf456a
Engelbert <Engelbert@DESKTOP-0B7JPLB>
2020-06-17 22:55:22 -0500

    Reportes

--- ProyectoFinal/Controllers/CursoController.cs
+++ ProyectoFinal/Controllers/CursoController.cs
@@ -7,6 +7,9 @@ using System.Web;
 using System.Web.Mvc;
 using CrystalDecisions.CrystalReports.Engine;
 using CrystalDecisions.Shared;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ProyectoFinal.Controllers
 {
@@ -16,20 +19,93 @@ namespace ProyectoFinal.Controllers
         ProyectoFinalEntities db = new ProyectoFinalEntities();
         string error = "";
 
+        public static clsCapaAcceso.AccesoDatos.SQLServer objSQL;
+
         // GET: Curso
         public ActionResult Index()
         {
+            
             List<Curso> c = db.Curso.AsNoTracking().ToList();            
             return View(c);            
         }
 
-        [HttpGet]
-        public void ExportDataProduccion()
+        //[HttpGet]
+        public ActionResult ExportDataProduccion()
         {
-            ReportDocument reportDocument = new ReportDocument();
-            string filePath = "C:\\Users\\ebaque\\Documents";
-            reportDocument.Load(filePath);
-            //CrystalReportViewer1.ReportSource = reportDocument;
+            ReportDocument rd = new ReportDocument();                        
+            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "prueba.rpt"));
+
+            TableLogOnInfos crtableLogoninfos = new TableLogOnInfos();
+            TableLogOnInfo crtableLogoninfo = new TableLogOnInfo();
+            ConnectionInfo crConnectionInfo = new ConnectionInfo();
+            Tables CrTables;
+
+            crConnectionInfo.ServerName = "35.222.165.220";
+            crConnectionInfo.DatabaseName = "ProyectoFinal";
+            crConnectionInfo.UserID = "admin";
+            crConnectionInfo.Password = "admin";
+
+            CrTables = rd.Database.Tables;
+
+            foreach (CrystalDecisions.CrystalReports.Engine.Table CrTable in CrTables)
+            {
+                crtableLogoninfo = CrTable.LogOnInfo;
+                crtableLogoninfo.ConnectionInfo = crConnectionInfo;
+                CrTable.ApplyLogOnInfo(crtableLogoninfo);
+            }
+
+            rd.SetDatabaseLogon("admin", "admin", "35.222.165.220", "ProyectoFinal");
+
+            string parameters = string.Empty;
+
+            DataSet dtsConsulta = new DataSet();
+            string ConnectionStrinHANA = "Data Source=35.222.165.220;Initial Catalog=ProyectoFinal;User ID=admin;Password=admin;Trusted_Connection=False;";
+            SqlConnection conn = new SqlConnection(ConnectionStrinHANA);
+            conn.Open();
+
+            string sQuery = "select IdUsuario from Usuario";
+
+            SqlCommand cmd = new SqlCommand(sQuery, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dtsConsulta);
+
+            conn.Close();
+
+            //string sQuery = "exec sprProDetalleProduccion ";
+            //parameters = string.Concat(parameters, "  @CCiMaquina = '", CCiMaquina, "'");
+            //parameters = string.Concat(parameters, ", @CCiMolde = '", CCiMolde, "'");
+            //parameters = string.Concat(parameters, ", @NNuIdOF = ", NNuIdOF);
+
+            //sQuery = string.Concat(sQuery, parameters);
+
+            //DataSet dtsConsulta = GlobalSQL.objSQL.TraerDataSetSql(sQuery);
+
+            //DataSet dtsConsulta = objSQL.TraerDataSetSql(sQuery);
+
+            //rd.SetParameterValue("@CCiMaquina", CCiMaquina);
+            //rd.SetParameterValue("@CCiMolde", CCiMolde);
+            //rd.SetParameterValue("@NNuIdOF", NNuIdOF);
+
+            rd.SetDataSource(dtsConsulta.Tables[0]);
+            rd.Refresh();
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            Stream stream1 = rd.ExportToStream(formatType: ExportFormatType.PortableDocFormat);
+            stream1.Seek(0, SeekOrigin.Begin);
+
+            rd.Close();
+            stream1.Flush(); //Always catches me out
+            stream1.Position = 0; //Not sure if this is required
+
+            string strDateNow = DateTime.Now.ToString("ddMMyyyy HH:mm:ss").ToString();
+            strDateNow = strDateNow.Replace(":", "");
+
+            string filName = string.Concat("Prueba ", strDateNow, ".pdf");
+
+            return File(stream1, "application/pdf", filName);
         }
 
             public ActionResult Edit(int id)
